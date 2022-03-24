dc6352f8879324c60c0f9984ebf5a84417c5abe2
HRNPH <51855316+GuideCH@users.noreply.github.com>
2022-03-24 20:29:56 +0700

    update replit deploy & add css

--- public/javascript/typewriter.js
+++ public/javascript/typewriter.js
@@ -1,15 +1,15 @@
-var i = 0;
-        var txt = '<Hirunkul Phimsiri/>'; /* The text */
-        var speed = 100; /* The speed/duration of the effect in milliseconds */
-        
-        function typeWriter() {
-          if (i < txt.length) {
-            document.getElementById("writename").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-          }
-        }
-        // call all onload function
-        window.addEventListener('load', (event) => {
-          typeWriter();
-        });
+async function typeWriter(messageToShow, targetElement, timeBetween, currentPos = 0) {
+    if (currentPos < messageToShow.length) {
+        document.getElementById(targetElement).innerHTML += messageToShow.charAt(currentPos);
+        currentPos++;
+        setTimeout(function () { typeWriter(messageToShow, targetElement, timeBetween, currentPos); }, timeBetween);
+    }
+    else{
+        return true;
+    }
+}
+// call all onload function
+window.addEventListener('load', (event) => {
+    typeWriter('<HIRUNKUL PHIMSIRI/>', 'writename', 100);
+    typeWriter('หิรัญกุล พิมพ์ศิริ', 'writename-thai', 200);
+});
