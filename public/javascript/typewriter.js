eec2c4bb4c030bff37fee162cf1a429165662b9d
HRNPH <51855316+GuideCH@users.noreply.github.com>
2022-03-25 07:37:35 +0700

    Fix icon size & use span to beautify Header Name

--- public/javascript/typewriter.js
+++ public/javascript/typewriter.js
@@ -2,14 +2,13 @@ async function typeWriter(messageToShow, targetElement, timeBetween, currentPos 
     if (currentPos < messageToShow.length) {
         document.getElementById(targetElement).innerHTML += messageToShow.charAt(currentPos);
         currentPos++;
-        setTimeout(function () { typeWriter(messageToShow, targetElement, timeBetween, currentPos); }, timeBetween);
-    }
-    else{
+        setTimeout(function() { typeWriter(messageToShow, targetElement, timeBetween, currentPos); }, timeBetween);
+    } else {
         return true;
     }
 }
 // call all onload function
 window.addEventListener('load', (event) => {
-    typeWriter('<HIRUNKUL PHIMSIRI/>', 'writename', 100);
+    typeWriter('HIRUNKUL PHIMSIRI', 'writename', 100);
     typeWriter('หิรัญกุล พิมพ์ศิริ', 'writename-thai', 200);
 });
